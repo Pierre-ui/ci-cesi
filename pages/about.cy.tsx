946ee0a0edbf5f068eed2a29a7adc554e02b9360
Pierre SCHELDEWAERT <pierre.scheldewaert@viacesi.fr>
2023-06-05 10:53:32 +0200

    feat(ci): testing ci

--- pages/about.cy.tsx
+++ pages/about.cy.tsx
@@ -3,6 +3,7 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
+import React from 'react';
 import About from './about'
 
 // Cypress Component Test
